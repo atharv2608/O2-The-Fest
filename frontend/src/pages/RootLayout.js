b023c17aa33a0d5ed55e1b4c9edfcb37c749ae82
codechini <arangathvenuchinmay@gmail.com>
2023-08-17 21:23:38 +0530

    globalStyling

--- frontend/src/pages/RootLayout.js
+++ frontend/src/pages/RootLayout.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+// import HomePage from "../pages/HomePage";
+import AuthenticationForm from "../components/AuthenticationForm";
 
 const RootLayout = () => {
   return (
@@ -8,6 +10,7 @@ const RootLayout = () => {
       <Navbar />
       <main>
         <Outlet />
+        <AuthenticationForm />
       </main>
     </>
   );
