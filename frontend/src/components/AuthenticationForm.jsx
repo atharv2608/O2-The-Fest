e5901adb634e6db7211d83e250bf3c0324c0ffa7
Rahul Nair <rahulsunilnair1074@gmail.com>
2023-08-21 15:10:07 +0530

    Login-signupDesign

--- frontend/src/components/AuthenticationForm.jsx
+++ frontend/src/components/AuthenticationForm.jsx
@@ -1,55 +1,73 @@
 import React from "react";
 import { Form, Link, useSearchParams } from "react-router-dom";
 import classes from "./AuthenticationForm.module.css";
+import { FcGoogle } from "react-icons/fc";
+
 
 const AuthenticationForm = () => {
   const [searchParams] = useSearchParams();
   const isLogin = searchParams.get("mode") === "login";
   return (
     <>
-      <Form className={classes.form} method='post'>
-        <h1>{isLogin ? "Log in" : "Sign Up"}</h1>
-        <p>
-          <label htmlFor='email'>Email</label>
+    <div className={classes.formContainer}>
+    <div className={classes.headingContainer}>
+   
+        <h1 className={`${!isLogin?classes.activated:'undefined'}`}> <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
+        Sign Up
+          </Link></h1>
+        <span>|</span>
+        <h1 className={`${isLogin?classes.activated:'undefined'}`}><Link to={`?mode=${isLogin ? "signup" : "login"}`}>
+        Log In
+          </Link></h1>
+        </div>
+    <Form className={classes.form} method='post'>
+      
+       
+      
+          <input
+            id='name'
+            type='text'
+            name='name'
+            placeholder='Name'
+            required
+          />
+    
           <input
             id='email'
             type='email'
             name='email'
-            placeholder='Enter email here'
+            placeholder='Email'
             required
           />
-        </p>
-        <p>
-          <label htmlFor='password'>Password</label>
+    
+      
           <input
             id='password'
             type='password'
             name='password'
-            placeholder='Enter your Password'
+            placeholder='Password'
             required
           />
-        </p>
+     
         {!isLogin && (
-          <p>
-            <label htmlFor='cpassword'>Confirm Password</label>
+         
             <input
               id='cpassword'
               type='password'
               name='cpassword'
-              placeholder='Confirm your Password'
+              placeholder='Confirm Password'
               required
             />
-          </p>
+     
         )}
         <div className={classes.actions}>
           <button > {isLogin ? "Login" : "Sign Up"}</button>
         </div>
-        <div className={classes.toggleLogin}>
-          <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
-            {isLogin ? "Signup" : "Login"}
-          </Link>
-        </div>
+          <p>OR</p>
+          <div><FcGoogle size={50}/></div>
       </Form>
+    </div>
+     
     </>
   );
 };
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,32 +1,12 @@
 import React from "react";
-import HomePageCSS from './HomePage.module.css';
+import HomePageCSS from "./HomePage.module.css";
 
 const HomePage = () => {
-  function sayHi() {
-    console.log("hi wahats upp")
-  }
   return (
     <>
-      <div className={HomePageCSS.container}>
-        hello
-      </div>
-      <div className={HomePageCSS.container}>
-        <button onClick={sayHi}>
-          <p className={HomePageCSS.txt}>Login</p>
-        </button>
-      </div>
-      <div className="container">
-        <div className={HomePageCSS.formContainer}>
-          <form action="">
-            <input type="text" name="" id="" />
-            <input type="number" name="" id="" />
-            <input type="email" name="" id="" />
-            <button>click me</button>
-          </form>
-        </div>
-      </div>
+      <div className={HomePageCSS.container}>hello</div>
     </>
-  )
+  );
 };
 
 export default HomePage;
