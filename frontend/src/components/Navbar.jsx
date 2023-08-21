cc35f45704db66c6b41f3e35d5a67f6b432890ed
Ayush Machha <ayushmac2002@gmail.com>
2023-08-22 00:46:21 +0530

    Navbar completed by Ayush

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,56 +3,55 @@ import navbarCss from "./Navbar.module.css";
 import { Twirl as Hamburger } from "hamburger-react";
 import logo from "../images/logo.png";
 import usrlogo from "../images/userimg.png";
+import myphoto from "../images/myphoto.jpeg";
+
 const Navbar = () => {
-  //dropdown open close state
+  //front end logic starts
   const [menuOpen, setMenuOpen] = useState(false);
-  const handleMenuToggle = (toggled) => {
-    if (toggled) {
-      setMenuOpen(true); // Open the menu
-    } else {
-      setMenuOpen(false); // Close the menu
-    }
+  const [userCardOpen, setUserCardOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
+    setUserCardOpen(false);
   };
 
-  //user card toggle
-  const [userDropdownOpen, setUserDropdownOpen] = useState(false);
-  // Prevent the propagation of the click event from the image
   const handleUserLogoClick = (event) => {
     event.stopPropagation();
-    setUserDropdownOpen(!userDropdownOpen);
+    setUserCardOpen(!userCardOpen);
+    setMenuOpen(false);
   };
 
-  // Add a ref to the card_container1
-  const cardContainerRef = useRef(null);
-
-  // Add a click event listener to the document
+  const headerRef = useRef(null);
   useEffect(() => {
     const handleClickOutside = (event) => {
-      if (
-        cardContainerRef.current &&
-        !cardContainerRef.current.contains(event.target)
-      ) {
-        setUserDropdownOpen(false); // Close the dropdown
+      if (headerRef.current && !headerRef.current.contains(event.target)) {
+        setUserCardOpen(false);
+        setMenuOpen(false);
       }
     };
-
     document.addEventListener("click", handleClickOutside);
-
     return () => {
       document.removeEventListener("click", handleClickOutside);
     };
   }, []);
+  //front end logic ends
+
+  //active tab logic would be done after applying routes
 
   // check here whether user signed in or not
-  const authsignin = true;
+  //temporary sign off logic
+  const [authsignin, setAuthSignIn] = useState(true);
 
   //if user signed in
   if (authsignin) {
     return (
-      <header className={navbarCss.header_container}>
+      <header className={navbarCss.header_container} ref={headerRef}>
         <nav className={navbarCss.container}>
           <div className={navbarCss.logo_container}>
-            <img src={logo} height="50px" width="50px" alt="" />
+            <a href="#">
+              {" "}
+              <img src={logo} height="50px" width="50px" alt="" />
+            </a>
           </div>
           <div className={navbarCss.links_container}>
             <a href="#" className={navbarCss.link}>
@@ -71,27 +70,41 @@ const Navbar = () => {
           <div className={navbarCss.ham_and_usrimg_container}>
             <div className={navbarCss.usrimg_container}>
               <img
-                src={usrlogo}
+                className={navbarCss.profile_img}
+                onClick={handleUserLogoClick}
+                src={myphoto}
                 height="40px"
                 width="40px"
-                onClick={handleUserLogoClick}
                 alt=""
               />
             </div>
             <div className={navbarCss.ham_container}>
-              <Hamburger size={20} onToggle={handleMenuToggle} />
+              <Hamburger
+                className={navbarCss.hamburger}
+                onToggle={handleMenuToggle}
+                toggled={menuOpen}
+                size={20}
+              />
             </div>
           </div>
         </nav>
-        {userDropdownOpen && (
-          <div className={navbarCss.card_container} ref={cardContainerRef}>
+        {userCardOpen && (
+          <div className={navbarCss.card_container}>
             <div className={navbarCss.dropdown_img_container}>
-              <img src={usrlogo} height="100px" width="100px" alt="" />
+              <img
+                className={navbarCss.profile_img}
+                src={myphoto}
+                height="100px"
+                width="100px"
+                alt=""
+              />
             </div>
             <div className={navbarCss.dropdown_details_and_btn_container}>
               <div className={navbarCss.dropdown_details_container}>
-                <div className={navbarCss.username_txt}>John Lorem Wick</div>
-                <div className={navbarCss.email_txt}>xyz@gmail.com</div>
+                <div className={navbarCss.username_txt}>John Wick</div>
+                <div className={navbarCss.email_txt}>
+                  mrwickbabayaga@gmail.com
+                </div>
               </div>
               <div className={navbarCss.dropdown_btn_container}>
                 <div>
@@ -100,7 +113,11 @@ const Navbar = () => {
                   </a>
                 </div>
                 <div>
-                  <a className={navbarCss.btn_logout} href="#">
+                  <a
+                    className={navbarCss.btn_logout}
+                    href="#"
+                    onClick={() => setAuthSignIn(false)}
+                  >
                     <i class="fa-solid fa-right-from-bracket"></i> Log Out
                   </a>
                 </div>
@@ -114,6 +131,7 @@ const Navbar = () => {
               <a href="#" className={navbarCss.dropdown_link}>
                 Home
               </a>
+
               <a href="#" className={navbarCss.dropdown_link}>
                 About Us
               </a>
@@ -132,7 +150,7 @@ const Navbar = () => {
   //if user is not signed in
   else {
     return (
-      <header>
+      <header className={navbarCss.header_container} ref={headerRef}>
         <nav className={navbarCss.container}>
           <div className={navbarCss.logo_container}>
             <img src={logo} height="50px" width="50px" alt="" />
@@ -153,7 +171,9 @@ const Navbar = () => {
           </div>
           <div className={navbarCss.ham_and_usrimg_container}>
             <div className={navbarCss.usrimg_container}>
+              {/* this image should send user login form and sign up form */}
               <img
+                className={navbarCss.profile_img}
                 src={usrlogo}
                 height="40px"
                 width="40px"
@@ -162,16 +182,22 @@ const Navbar = () => {
               />
             </div>
             <div className={navbarCss.ham_container}>
-              <Hamburger size={20} onToggle={handleMenuToggle} />
+              <Hamburger
+                size={20}
+                onToggle={handleMenuToggle}
+                toggled={menuOpen}
+              />
             </div>
           </div>
         </nav>
+
         {menuOpen && (
           <nav className={navbarCss.dropdown_container}>
             <div className={navbarCss.dropdown_links_container}>
               <a href="#" className={navbarCss.dropdown_link}>
                 Home
               </a>
+
               <a href="#" className={navbarCss.dropdown_link}>
                 About Us
               </a>
