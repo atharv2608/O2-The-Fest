8a8b0fdcdc0dbb5e4ecb91c594ea07ee6982a944
Atharv Lingayat <76042372+atharv2608@users.noreply.github.com>
2023-11-01 15:22:29 +0530

    Home page and About O2 page created. Also, I have changed the navbar position as fixed

--- frontend/src/components/AboutO2.jsx
+++ frontend/src/components/AboutO2.jsx
@@ -1,34 +1,34 @@
-import React from 'react'
-import AboutO2Css from './AboutO2.module.css'
-
-const AboutO2 = () => {
+import React from "react";
+import style from "./AboutO2.module.css";
+import logo from "../images/logo.png";
+import Countdown from "./Countdown";
+export default function AboutO2() {
   return (
-    <>
-      <div className={AboutO2Css.container}>
-        <div className={AboutO2Css.content}>
-          <h1>O2 The Fest</h1>
+    <section className={style.aboutSection}>
+      <div className={style.countdown}>
+        <Countdown />
+      </div>
+
+      <div className={style.aboutContainer}>
+        
+        <div className={style.logoContainer}>
+          <img
+            src={logo}
+            alt="Unable to load image. Check your internt connection!"
+          />
         </div>
 
-        <div className={AboutO2Css.content}>
-          <h2>Fire needs 'oxygen' to burn and spread heat, while we
-            need O2 to 'Energize and Rise'. It was considered as
-            one of the best festivals in the city with the motto
-            "Breathe it! Live it! Love it!" O2 is known to celebrate
-            talent, potential, direct energy and dedication
-            towards something big. It has always enhanced talent
-            and created a sense of responsibility among the
-            youth. We took of on our journey in 2006, and we
-            have come a long way!
-            O2 has scaled heights much beyond everyone's
-            expectations, thus setting exceptional standards. A
-            salute to the enthusiastic young souls! Bhavan's College provides a pre-eminent
-            place to unveil your hidden talent in various fields like PA, LA, FA, Sports, Informals
-            Media & Management. O<sub>2</sub> The Fest, Cultural Fest of Bhavan's COllege is back after 8 Years with new energy and enthusiasm. We welcome you to 'O<sub>2</sub> THE FEST..... WHERE LIFE BEGINS!
-          </h2>
+        <div className={style.aboutTextContent}>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit
+            pariatur quas provident quia facilis incidunt repudiandae! Sunt sed
+            fugiat reiciendis! Ratione quasi quisquam pariatur praesentium?
+            Corporis recusandae odio doloremque, provident quasi eius vero
+            aspernatur soluta beatae! Molestiae earum, id ut obcaecati illo eos
+            tenetur mollitia.
+          </p>
         </div>
       </div>
-    </>
-  )
+    </section>
+  );
 }
-
-export default AboutO2
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,17 +2,22 @@ import React from "react";
 import AboutO2 from "../components/AboutO2";
 import Countdown from "../components/Countdown";
 import style from "../components/HomePage.module.css";
+import homeVideo from "../videos/home-bg-v1.mp4";
 
 const HomePage = () => {
   return (
-    <section className={style.homeSection}>
-      <div className={style.textContainer}>
-      <div className={style.subTitle}>That time of year has came</div>
-      <div className={style.heading}><h1>O<sub>2</sub> The Fest</h1></div>
-      <div className={style.subTitle}>Where Fire meets oxygen</div>
-      <Countdown />
-      </div>
-    </section>
+    <>
+      <section>
+        <div className={style.videoContainer}>
+          <video autoPlay muted loop className={style.backgroundVideo}>
+            <source src={homeVideo} type="video/mp4" />
+          </video>
+        </div>
+        <div className={style.homeContent}>
+        </div>
+      </section>
+      <AboutO2 />
+    </>
   );
 };
 
